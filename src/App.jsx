c20fc3800bc5ef9b5588ba8d2e0889bc3229d625
MariannaPereira <maricerqueira6567@gmail.com>
2024-02-28 18:16:11 -0400

    desafio VNW

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "./Componentes/Header/Header.jsx"
+
+function App(){
+  return(
+<Header/>
+  )
+}
+
+export default App 
---
+++ src/Componentes/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Header/Header.css"
+
+function Header(){
+    return(
+        <header>
+            <p>Marianna Cerqueira, a mais linda</p>
+        </header>
+    )
+}
+
+export default Header
